578e6bc215032db6504cb7cb012961242c2b4bac
Wasealex <wasealex27@gmail.com>
2024-06-30 11:32:35 +0300

    Refactor pascal_triangle.py to return a list of lists for n triangles

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """modul pascal trialgle"""
 if __name__ == "__main__":
+    """run directly"""
     def pascal_triangle(n):
-        """def of pascal trialngle"""
+        """def of pascal trialngle
+        return list of lists of n triangle
+        return empty if n < 0
+        
+        """
         if n <= 0:
             return []
-        return [[n]] 
+        return [n] 
