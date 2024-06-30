ab06d59db0c5edf258945aaecf63a37c515f33a6
Wasealex <wasealex27@gmail.com>
2024-06-30 11:48:50 +0300

    chore: Refactor pascal_triangle.py to return a list of lists for n triangles

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""modul pascal trialgle"""
+"""Module Pascal Triangle"""
+
+
 def pascal_triangle(n):
-    """def of pascal trialngle
-        return list of lists of n triangle
-        return empty if n < 0
-        
+    """Definition of Pascal Triangle.
+    Returns a list of lists representing the Pascal Triangle of size n.
+    Returns an empty list if n < 0.
     """
     pascal = []
     if n <= 0:
         return pascal
-        
-        
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
         pascal.append(row)
-        
+
     return pascal
