5c68b8403c8d7435a92fb4b2e5382462401e2664
Wasealex <wasealex27@gmail.com>
2024-06-30 11:42:21 +0300

    chore: Refactor pascal_triangle.py to return a list of lists for n triangles

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,6 +8,15 @@ if __name__ == "__main__":
         return empty if n < 0
         
         """
+        pascal = []
         if n <= 0:
-            return []
-        return [n] 
+            return pascal
+        
+        
+        for i in range(n):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+            pascal.append(row)
+        
+        return pascal
