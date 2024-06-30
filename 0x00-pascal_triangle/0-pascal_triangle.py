3f056c9b7c11f53fa66a49ab8dd1913ab8a4dd72
Wasealex <wasealex27@gmail.com>
2024-06-30 11:46:31 +0300

    indirect

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """modul pascal trialgle"""
-if __name__ == "__main__":
-    """run directly"""
-    def pascal_triangle(n):
-        """def of pascal trialngle
+def pascal_triangle(n):
+    """def of pascal trialngle
         return list of lists of n triangle
         return empty if n < 0
         
-        """
-        pascal = []
-        if n <= 0:
-            return pascal
-        
-        
-        for i in range(n):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
-            pascal.append(row)
-        
+    """
+    pascal = []
+    if n <= 0:
         return pascal
+        
+        
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        pascal.append(row)
+        
+    return pascal
